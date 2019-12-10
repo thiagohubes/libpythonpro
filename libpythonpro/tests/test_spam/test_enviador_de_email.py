c4f1659bc14b128ffa88c0319c68a73346b12017
thiagohubes <thiago.hubes@gmail.com>
2019-12-09 21:35:38 -0300

    Criados testes e arquivos db.py, enviador_de_email.py e modelos.py

---
+++ libpythonpro/tests/test_spam/test_enviador_de_email.py
@@ -0,0 +1,38 @@
+import pytest
+
+from libpythonpro.spam.enviador_de_email import Enviador, EmailInvalido
+
+
+def test_criar_enviador_de_email():
+    enviador = Enviador()
+    assert enviador is not None
+
+
+@pytest.mark.parametrize(
+    'destinatario',
+    ['thiago.hubes@gmail.com', 'foo@bar.com.br'],
+
+)
+def test_remetente(destinatario):
+    enviador = Enviador()
+    destinatarios = ['thiago.hubes@gmail.com', 'foo@bar.com.br']
+    resultado = enviador.enviar(
+        destinatario,
+        'thiago.hubes@outlook.com',
+        'Cursos Python Pro',
+        'Primeira turma Guido von Rossum aberta.')
+    assert destinatario in resultado
+
+
+@pytest.mark.parametrize(
+    'remetente',
+    ['', 'foo'],
+)
+def test_remetente_invalido(remetente):
+    enviador = Enviador()
+    with pytest.raises(EmailInvalido):
+        enviador.enviar(
+            remetente,
+            'thiago.hubes@outlook.com',
+            'Cursos Python Pro',
+            'Primeira turma Guido von Rossum aberta.')
