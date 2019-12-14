d7769fe1b16190223dbe0ea71c1b39c51acd672e
thiagohubes <thiago.hubes@gmail.com>
2019-12-14 04:08:46 -0300

    Alterado teste de envio de spam para testar quantidade de usuários testados com teste parametrizado.

--- libpythonpro/tests/test_spam/test_usuarios.py
+++ libpythonpro/tests/test_spam/test_usuarios.py
@@ -2,13 +2,16 @@ from libpythonpro.spam.modelos import Usuario
 
 
 def test_salvar_usuario(sessao):
-    usuario = Usuario(nome='Thiago')
+    usuario = Usuario(nome='Thiago', email="thiago.hubes@gmail.com")
     sessao.salvar(usuario)
     assert isinstance(usuario.id, int)
 
 
 def test_listar_usuarios(sessao):
-    usuarios = [Usuario(nome='Thiago'), Usuario(nome='Hubes')]
+    usuarios = [
+        Usuario(nome='Thiago', email="thiago.hubes@gmail.com"),
+        Usuario(nome='Hubes', email="thiago.hubes@gmail.com")
+    ]
     for usuario in usuarios:
         sessao.salvar(usuario)
     assert usuarios == sessao.listar()
