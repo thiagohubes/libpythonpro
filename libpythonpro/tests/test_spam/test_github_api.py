8fdc2088ca8344a0f5933c2495e1e69d3c50bf2f
thiagohubes <thiago.hubes@gmail.com>
2019-12-20 03:58:58 -0300

    Feitos testes com mocker

--- libpythonpro/tests/test_spam/test_github_api.py
+++ libpythonpro/tests/test_spam/test_github_api.py
@@ -1,14 +1,28 @@
 from unittest.mock import Mock
 
+import pytest
+
 from libpythonpro import github_api
 
 
-def test_buscar_avatar():
+@pytest.fixture
+def avatar_url(mocker):
     resp_mock = Mock()
+    url = 'https://avatars3.githubusercontent.com/u/50760362?v=4'
     resp_mock.json.return_value = {
-        'login': 'thiagohubes', 'id': 50760362, 'node_id': 'MDQ6VXNlcjUwNzYwMzYy',
-        'avatar_url': 'https://avatars3.githubusercontent.com/u/50760362?v=4',
+        'login': 'thiagohubes', 'id': 50760362,
+        'avatar_url': url,
     }
-    github_api.requests.get = Mock(return_value=resp_mock)
+    get_mock = mocker.patch('libpythonpro.github_api.requests.get')
+    get_mock.return_value = resp_mock
+    return url
+
+
+def test_buscar_avatar(avatar_url):
     url = github_api.buscar_avatar('thiagohubes')
-    assert 'https://avatars3.githubusercontent.com/u/50760362?v=4' == url
+    assert avatar_url == url
+
+
+def test_buscar_avatar_integracao():
+    url = github_api.buscar_avatar('thehubes')
+    assert 'https://avatars2.githubusercontent.com/u/57348569?v=4' == url
